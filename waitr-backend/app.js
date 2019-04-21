28fffdade41c968041761bc47802e9bd3e4664dc
Paulo Ribeiro <paulocesarribeiro89@gmail.com>
2019-04-21 00:58:24 -0400

    Hopefully provided fix for heroku problem

--- waitr-backend/app.js
+++ waitr-backend/app.js
@@ -38,8 +38,11 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, `client/build`)));
-// app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "build/index.html"));
+});
 
 app.use("/waitrs", waitrsRouter);
 app.use("/events", eventsRouter);
