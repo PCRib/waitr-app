256124b0e89eaea2241b2c30c20bff5c6cb0ba60
Paulo Ribeiro <paulocesarribeiro89@gmail.com>
2019-04-21 00:24:56 -0400

    add changes for deployment

--- waitr-backend/app.js
+++ waitr-backend/app.js
@@ -1,45 +1,48 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const mongoose = require('mongoose');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
 // const cors = require('cors')
 
-
-var waitrsRouter = require('./routes/waitrs');
-var eventsRouter = require('./routes/events');
-var addingIdsRouter = require('./routes/addingIds');
+var waitrsRouter = require("./routes/waitrs");
+var eventsRouter = require("./routes/events");
+var addingIdsRouter = require("./routes/addingIds");
 
 var app = express();
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    next();
-  });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+  next();
+});
 
-
-mongoose.connect('mongodb://localhost/waitr-app');
+mongoose.connect(
+  "mongodb://pcrib:pcrman28@ds145356.mlab.com:45356/heroku_vn7lgslg"
+);
 
 // Tell Mongoose to use ES6 Promises for its promises
 mongoose.Promise = global.Promise;
 
 // Log to console any errors or a successful connection.
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-	console.log("Connected to db at /waitr-app")
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to db at /waitr-app");
 });
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, `client/build`)));
 // app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use('/waitrs', waitrsRouter);
-app.use('/events', eventsRouter);
-app.use('/addingIds', addingIdsRouter);
+app.use("/waitrs", waitrsRouter);
+app.use("/events", eventsRouter);
+app.use("/addingIds", addingIdsRouter);
 
 module.exports = app;
--- waitr-backend/routes/events.js
+++ waitr-backend/routes/events.js
@@ -1,72 +1,69 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const Events = require("../models/Events")
-const Waitrs = require("../models/Waitrs")
-
-
-
+const Events = require("../models/Events");
+const Waitrs = require("../models/Waitrs");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
   Events.find({})
-  .then(events => {
-    res.json(events)
-  })
-  .catch(error => {
-    res.status(500).send(error)
-  })
+    .then(events => {
+      res.json(events);
+      console.log(events);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
 });
 
-router.get('/:id', function(req, res, next) {
-  const id = req.params.id
-  Events.findById({_id:id})
-  .populate('waitrs_scheduled')
-  .then(event => {
-    res.json(event)
-  })
-  .catch(error => {
-    res.status(500).send(error)
-  })
+router.get("/:id", function(req, res, next) {
+  const id = req.params.id;
+  Events.findById({ _id: id })
+    .populate("waitrs_scheduled")
+    .then(event => {
+      res.json(event);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
 });
 
-router.post('/', function(req, res, next) {
-  const data = req.body
+router.post("/", function(req, res, next) {
+  const data = req.body;
   let newEvent = Events({
     event_date: data.event_date,
     event_time: data.event_time,
     event_price: data.event_price,
     event_location: data.event_location,
     event_address: data.event_address,
-    event_client:data.event_client,
+    event_client: data.event_client,
     event_type: data.event_type,
     attire: data.attire,
     hourly_rate: data.hourly_rate,
-    waitrs_scheduled:[]
-  }
-  )
-  newEvent.save()
-  .then(savedEvent => {
-    res.json(savedEvent)
-  })
-  .catch(error => {
-    res.status(500).send(error)
-  })
+    waitrs_scheduled: []
+  });
+  newEvent
+    .save()
+    .then(savedEvent => {
+      res.json(savedEvent);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
 });
 
-router.put('/addingIds', function(req, res, next) {
+router.put("/addingIds", function(req, res, next) {
   const eventId = req.body.currentEvent;
   const waitrId = req.body.currentWaitr;
-  Events.findById({_id:eventId})
-  .then(event => {
-    // Waitrs.findById({_id:waitrId})
-    event.waitrs_scheduled.push(waitrId);
-    event.save()
-    .then(result => {
-      res.json(result)
+  Events.findById({ _id: eventId })
+    .then(event => {
+      // Waitrs.findById({_id:waitrId})
+      event.waitrs_scheduled.push(waitrId);
+      event.save().then(result => {
+        res.json(result);
+      });
     })
-  })
-  .catch(error => {
-    res.status(500).send(error)
-  })
+    .catch(error => {
+      res.status(500).send(error);
+    });
 });
 module.exports = router;
